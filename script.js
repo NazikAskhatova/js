acb46ffd2d3ec7dfb8e3827bf76b8a5f7080665e
wiusxt <ashatovanas65@gmail.com>
2021-12-20 23:45:13 +0600

    5 math examples with console.log was added

---
+++ script.js
@@ -0,0 +1,125 @@
+// alert("hello world");
+// console.log("hello lily!");
+
+// declaration 
+var firstName; // (переменная)
+// var lastName;
+// var age;
+
+// initialize
+var lastName = "Volkov"; // (значение переменной)
+
+// assigment
+firstName = "Vasya"; // (существует переменная ферст нэйм)
+
+//  declaration
+var lastName;
+
+// declaration
+// var name;
+// var lessonsName;
+// var address;
+// var age;
+// var data;
+// var status; 
+// var favoriteColors;
+// var hobby; 
+// var mothersName;
+
+
+// initialize
+var name = "Nazik";
+var lessonsName = "Javascript";
+var address = "Voshod";
+var age = "17";
+var data = "21.12.21";
+var status = "ready to work";
+var favoriteColors = "White, Green";
+var hoddy = "kpop, read mangas";
+var mothersName = "Munara";
+var sister = "Salta";
+var brothers = "Azat";
+var family = "dad, mom, me and lil bro and sis";
+var besties = "Syrgo, Taazim, Jennie Kim";
+var countries = "Australia";
+var gf = "Rose";
+var favoriteAnime = "Atack on titan";
+var music = "kpop, jpop, classic";
+var group = "bts";
+var enhypen = "sunoo, jake";
+var txt = "bom, soobin";
+var bts = "namoo";
+var blackpink = "rose";
+var lanaDelRey = "best.";
+var twice = "chaeyong";
+var ash = "love!";
+var bsd = "aku";
+
+// example 1 math
+var potatoes = 5, tomatoes = 3;
+var vegetables = potatoes + tomatoes;
+var result = vegetables / 2;
+// console.log(result); (показывает на консули)
+
+var books = 10, newslaters = 8;
+var buys = books + newslaters;
+var result = buys / 6;
+
+var albums = 55, photocards = 11;
+var all = albums * photocards
+var findAnswer = all - 100
+// alert(findAnswer);
+
+var peaches = 100, cherry = 498;
+var fruits = cherry - peaches;
+var result = fruits / 2;
+alert(result);
+
+
+// example 2 math
+result = (2 + 1) / 3 - 6 * 2
+result = (20 + 40) / 5 * 2
+result = 45 - 25 / (30 - 25)
+// alert(result);
+
+// 3 / 3 - 6 * 2
+//  1 - 12
+// -11
+// console.log(result);
+
+var a = 5;
+var b = 6;
+var rectangleArea = a * b; //(10)
+
+var PI = 3.14;
+var r = 5;
+var circleArea = r * r * PI;
+// console.log(circleArea);
+// alert(circleArea); (в вышедшем окне)
+ 
+let eight = Math.sqrt(4)
+console.log(eight);
+
+var PI = 5.18;
+var r = 2;
+var circleArea = r * r * PI;
+console.log(circleArea);
+
+var a = 7;
+var b = 9;
+var c = 15;
+var summary = a * b + c;
+console.log(summary);
+
+var a = 17;
+var b = 28;
+var c = 34;
+var d = 43;
+var findFormula = a * b * c / d;
+console.log(findFormula);
+
+var f = 45;
+var d = 38;
+var g = 27;
+var answer = f / d - g
+console.log(answer);
